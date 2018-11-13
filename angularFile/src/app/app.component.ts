e46ec0fef4ee9decdcdaba8085a14f36407520dc
lbs <lbs@nb>
2018-11-13 01:06:20 -0500

    Finishing

--- angularFile/src/app/app.component.ts
+++ angularFile/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularFile';
+  title = 'Compare File';
+  description = 'Compare file using Spring boot and Angular';
 }
--- angularFile/src/app/app.module.ts
+++ angularFile/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FormUploadComponent } from './upload/form-upload/form-upload.component';
+import { ListUploadComponent } from './upload/list-upload/list-upload.component';
+import { DetailsUploadComponent } from './upload/details-upload/details-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormUploadComponent,
+    ListUploadComponent,
+    DetailsUploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
